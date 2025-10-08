0bb6935f20b297c40a371de1dc1c27aabd2ebfb7
TheRealMcCall <lamccall1983@gmail.com>
2025-10-08 16:33:24 +0100

    Add category model and register in admin

--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=120,
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=120,
+        unique=True
+    )
+    description = models.TextField(
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+    
