36e1c677894791c30b24d87ac89013af98169ac1
TheRealMcCall <lamccall1983@gmail.com>
2025-10-09 12:59:25 +0100

    Feat: Add Address model and register in admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Address
 
 # Register your models here.
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user',
+                    "first_name",
+                    "last_name",
+                    "city",
+                    "postcode",
+                    "country",
+                    "default_address"
+                    )
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,51 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Address(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="addresses"
+        )
+    first_name = models.CharField(
+        max_length=30
+        )
+    last_name = models.CharField(
+        max_length=30
+        )
+    first_line = models.CharField(
+        max_length=80
+        )
+    second_line = models.CharField(
+        max_length=80,
+        blank=True
+        )
+    city = models.CharField(
+        max_length=30
+        )
+    postcode = models.CharField(
+        max_length=20
+        )
+    country = models.CharField(
+        max_length=25,
+        default="United Kingdom"
+        )
+    phone_number = models.CharField(
+        max_length=30,
+        blank=True
+        )
+    default_address = models.BooleanField(
+        default=False
+        )
+    billing_address = models.BooleanField(
+        default=False
+        )
+    delivery_address = models.BooleanField(
+        default=False
+        )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
