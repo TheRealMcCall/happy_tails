b22801af8df704320adb5b766239d4b7e3b1d388
TheRealMcCall <lamccall1983@gmail.com>
2025-10-11 17:48:48 +0100

    Add placeholder checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def checkout_view(request):
+    return render(request, "checkout/checkout.html")
--- happy_tails/urls.py
+++ happy_tails/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from checkout import views as checkout_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("basket/", include("basket.urls")),
+    path("checkout/", include("checkout.urls")),
     path("accounts/", include("allauth.urls")),
     path("", include(("store.urls", "store"), namespace="store")),
 ]
