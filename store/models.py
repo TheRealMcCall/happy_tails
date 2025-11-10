cfdd78fc69afa10e2c743649f9f360f1892ed7ec
TheRealMcCall <lamccall1983@gmail.com>
2025-11-10 16:36:12 +0000

    Refactor: remove unused image field from Variant model

--- store/models.py
+++ store/models.py
@@ -70,11 +70,6 @@ class Variant(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="variants"
         )
-    image = CloudinaryField(
-        "image",
-        blank=True,
-        null=True
-        )
     size = models.CharField(
         max_length=60,
         blank=True
