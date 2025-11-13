9fc759ffbc628202b6f30122e4b86ebe7187ccc9
TheRealMcCall <lamccall1983@gmail.com>
2025-11-13 11:35:17 +0000

    Test: Verify checkout redirects to basket when empty

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CheckoutViewTests(TestCase):
+    """Tests for the checkout view behaviour."""
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username="checkout_user",
+            email="checkout@example.com",
+            password="testpass123",
+        )
+
+    def test_checkout_redirects_when_basket_empty(self):
+        """
+        If the basket is empty, checkout_view should redirect to
+        the basket:view_basket page.
+        """
+        # login_required decorator means we must be logged in first
+        self.client.force_login(self.user)
+
+        url = reverse("checkout:start")
+        response = self.client.get(url)
+
+        self.assertRedirects(response, reverse("basket:view_basket"))
